0c419f27196f31734476ab6324136fc68380d92c
Raul Lourenco <raulourenco@hotmail.com>
2026-01-08 20:54:00 -0300

    feat: slices, maps, functions, multiple return values and variadic functions

--- go_by_example.go
+++ go_by_example.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"math"
 	"time"
+	"slices"
+	"maps"
 )
 
 const s string = "constant"
@@ -181,6 +183,138 @@ func arrays() {
 	fmt.Println("2d: ", twoD)
 }
 
+func sliceArrays() {
+	var s []string
+	fmt.Println("uninit:", s, s == nil, len(s) == 0)
+
+	s = make([]string, 3)
+	fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
+
+	fmt.Println("len", len(s))
+
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("apd", s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy", c)
+
+	l := s[2:5]
+	fmt.Println("sl1", l)
+
+	l = s[:5]
+	fmt.Println("sl2", l)
+
+	l = s[2:]
+	fmt.Println("sl3", l)
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl", t)
+
+	t2 := []string{"g", "h", "i"}
+	if slices.Equal(t, t2) {
+		fmt.Println("t == t2")
+	}
+
+	twoD := make([][]int, 3)
+	for i := range 3 {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := range innerLen {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
+}
+
+func Maps() {
+	m := make(map[string]int)
+	
+	m["k1"] = 7
+	m["k2"] = 13
+	
+	fmt.Println("map:", m)
+
+	v1 := m["k1"]
+	fmt.Println("v1:", v1)
+
+	v3 := m["k3"]
+	fmt.Println("v3:", v3)
+	
+	fmt.Println("len:", len(m))
+
+	delete(m, "k2")
+	fmt.Println("map:", m)
+	
+	clear(m)
+	fmt.Println("map:", m)
+	
+	_, prs := m["k2"]
+	fmt.Println("prs:", prs)
+	
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map:", n)
+	
+	n2 := map[string]int{"foo": 1, "bar": 2}
+	if maps.Equal(n, n2) {
+		fmt.Println("n == n2")
+	}
+}
+
+func plus(a int, b int) int {
+	return a + b
+}
+
+func plusPlus(a, b, c int) int {
+	return a + b + c
+}
+
+func functions() {
+	res := plus(1, 2)
+	fmt.Println("1+2 =", res)
+
+	res = plusPlus(1, 2, 3)
+	fmt.Println("1+2+3 =", res)
+}
+
+func vals() (int, int) {
+	return 3, 7
+}
+
+func multipleReturnValues() {
+	a, b := vals()
+	fmt.Println(a)
+	fmt.Println(b)
+	
+	_, c := vals()
+	fmt.Println(c)
+}
+
+func sum(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+		
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
+}
+
+func variadicFunctions() {
+	sum(1, 2)
+	sum(1, 2, 3)
+	
+	nums := []int{1, 2, 3, 4}
+	sum(nums...)
+}
+
 func main() {
 	helloWorld()
 	values()
@@ -190,4 +324,9 @@ func main() {
 	ifElse()
 	switches()
 	arrays()
+	sliceArrays()
+	Maps()
+	functions()
+	multipleReturnValues()
+	variadicFunctions()
 }
