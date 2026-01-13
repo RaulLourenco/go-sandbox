04157c81fe6ea766ccfe6eb65ca069f5fa810f8a
Raul Lourenco <raulourenco@hotmail.com>
2026-01-12 23:54:12 -0300

    feat: structs and methods

--- go_by_example.go
+++ go_by_example.go
@@ -440,6 +440,70 @@ func examineRune(r rune) {
 	}
 }
 
+type person struct {
+	name string
+	age int
+}
+
+func newPerson(name string) *person {
+	p := person{name: name}
+	p.age = 42
+	return &p
+}
+
+func structs() {
+	fmt.Println(person{"Bob", 20})
+	
+	fmt.Println(person{name: "Alice", age: 30})
+	
+	fmt.Println(person{name: "Fred"})
+	
+	fmt.Println(&person{name: "Ann", age: 40})
+	
+	fmt.Println(newPerson("Jon"))
+
+	s := person{name: "Sean", age: 50}
+	fmt.Println(s.name)
+
+	sp := &s
+	fmt.Println(sp.age)
+
+	sp.age = 51
+	fmt.Println(sp.age)
+
+	dog := struct {
+		name   string
+		isGood bool
+	}{
+		"Rex",
+		true,
+	}
+	fmt.Println(dog)
+}
+
+type rect struct {
+	width, height int
+}
+
+func (r *rect) area() int {
+	return r.width * r.height
+}
+
+func (r rect) perim() int {
+	return 2*r.width + 2*r.height
+}
+
+func methods() {
+	r := rect{width: 10, height: 5}
+	
+	fmt.Println("area: ", r.area())
+	fmt.Println("perim: ", r.perim())
+	
+	rp := &r
+	fmt.Println("area: ", rp.area())
+	fmt.Println("perim: ", rp.perim())
+}
+
 func main() {
 	helloWorld()
 	values()
@@ -459,4 +523,6 @@ func main() {
 	rangeOverBuiltInTypes()
 	pointers()
 	stringsAndRunes()
+	structs()
+	methods()
 }
