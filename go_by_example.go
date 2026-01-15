1abc1b90549049c4a09bbdc7492951486edae4c7
Raul Lourenco <raulourenco@hotmail.com>
2026-01-14 23:57:20 -0300

    feat: range over iterators

--- go_by_example.go
+++ go_by_example.go
@@ -7,6 +7,7 @@ import (
 	"slices"
 	"maps"
 	"unicode/utf8"
+	"iter"
 )
 
 const s string = "constant"
@@ -682,6 +683,50 @@ func generics() {
 	fmt.Println("list:", lst.AllElements())
 }
 
+func (lst *List[T]) All() iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for e := lst.head; e != nil; e = e.next {
+			if !yield(e.val) {
+				return
+			}
+		}
+	}
+}
+
+func genFib() iter.Seq[int] {
+	return func(yield func(int) bool) {
+		a, b := 1, 1
+		
+		for {
+			if !yield(a) {
+				return
+			}
+			a, b = b, a+b
+		}
+	}
+}
+
+func rangeOverIterators() {
+	lst := List[int]{}
+	lst.Push(10)
+	lst.Push(13)
+	lst.Push(23)
+	
+	for e := range lst.All() {
+		fmt.Println(e)
+	}
+
+	all := slices.Collect(lst.All())
+	fmt.Println("all:", all)
+	
+	for n := range genFib() {
+		if n >= 10 {
+			break
+		}
+		fmt.Println(n)
+	}
+}
+
 func main() {
 	helloWorld()
 	values()
@@ -707,4 +752,5 @@ func main() {
 	enums()
 	structEmbedding()
 	generics()
+	rangeOverIterators()
 }
