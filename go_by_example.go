c4552028893867d4756c3721045fa2a7c848db3c
Raul Lourenco <raulourenco@hotmail.com>
2026-01-16 23:51:34 -0300

    feat: non blocking channel operations

--- go_by_example.go
+++ go_by_example.go
@@ -919,6 +919,35 @@ func timeouts() {
 	}
 }
 
+func nonBlockingChannelOperations() {
+	messages := make(chan string)
+	signals := make(chan bool)
+	
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	default:
+		fmt.Println("no message received")
+	}
+	
+	msg := "hi"
+	select {
+	case messages <- msg:
+		fmt.Println("sent message", msg)
+	default:
+		fmt.Println("no message sent")
+	}
+	
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	case sig := <-signals:
+		fmt.Println("received signal", sig)
+	default:
+		fmt.Println("no activity")
+	}
+}
+
 func main() {
 	helloWorld()
 	values()
@@ -954,4 +983,5 @@ func main() {
 	channelDirections()
 	selectFunc()
 	timeouts()
+	nonBlockingChannelOperations()
 }
